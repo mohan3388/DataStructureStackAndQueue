038982fefde6e4b3819508380dbeac0345b828aa
mohan338 <sahumohan338@gmail.com>
2022-05-03 18:18:56 +0530

    [mohan]add:stack queue

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StackAndQueue;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        StackAndQueueProblem stack = new StackAndQueueProblem();
+        stack.Push(70);
+        stack.Push(30);
+        stack.Push(56);
+        stack.Display();
+    }
+}
